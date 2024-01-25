db2a271344ef3a113bd6e0a615cad30bfae0aa28
Adam Fauzan <adamfzan@gmail.com>
2024-01-25 11:03:22 +0700

    fixing

--- lib/view/media/media_page.dart
+++ lib/view/media/media_page.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/model/media_model.dart';
 import 'package:flutter_application_1/view/media/media_provider.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:provider/provider.dart';
--- lib/view/media/media_provider.dart
+++ lib/view/media/media_provider.dart
@@ -1,40 +1,11 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/model/media_model.dart';
 
 class MediaProvider extends ChangeNotifier {
-  // List<Media> mediaItems = [];
-
   String? getCurrentUserId() {
     return FirebaseAuth.instance.currentUser?.uid;
   }
-
-  // CollectionReference get mediaCollection {
-  //   return FirebaseFirestore.instance.collection('media');
-  // }
-
-  // Future<void> addMedia(Media media) async {
-  //   try {
-  //     String? userId = getCurrentUserId();
-  //     if (userId != null) {
-  //       DocumentReference documentReference = await mediaCollection
-  //           .doc(userId)
-  //           .collection('userMedia')
-  //           .add(media.toMap());
-
-  //       String mediaId = documentReference.id;
-  //       mediaItems.add(Media(
-  //           id: mediaId, type: media.type, file: media.file, url: media.url));
-  //       notifyListeners();
-  //     } else {
-  //       print('User not authenticated');
-  //     }
-  //   } catch (error) {
-  //     print('Failed to add media to Firestore: $error');
-  //   }
-  // }
 
   List<String> fileUrls = [];
 
